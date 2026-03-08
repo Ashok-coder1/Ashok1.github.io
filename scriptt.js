1b95d3ef4defcfa9728e8e971f500000043332b0
Ashok-coder1 <homekhabar@gmail.com>
2026-03-08 18:58:27 +0545

    Refactor signup and login functions for clarity

--- scriptt.js
+++ scriptt.js
@@ -1,55 +1,27 @@
-const API = "https://ashok1-github-io-3.onrender.com";
+const API = "https://ashok1-github-io-3.onrender.com"; // replace with your Render URL
 
-function signup(){
+function signup() {
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
 
-const email = document.getElementById("signupEmail").value;
-const password = document.getElementById("signupPassword").value;
-
-fetch(API + "/signup",{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body: JSON.stringify({
-email:email,
-password:password
-})
-
-})
-
-.then(res=>res.json())
-.then(data=>{
-alert(data.message)
-})
-
+  fetch(API + "/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message));
 }
 
-function login(){
+function login() {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
 
-const email = document.getElementById("loginEmail").value;
-const password = document.getElementById("loginPassword").value;
-
-fetch(API + "/login",{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body: JSON.stringify({
-email:email,
-password:password
-})
-
-})
-
-.then(res=>res.json())
-.then(data=>{
-alert(data.message)
-})
-
+  fetch(API + "/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message));
 }
