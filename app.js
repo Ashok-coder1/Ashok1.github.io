96b0ed0d7807c8671b3def583486c55397edd3ae
Ashok-coder1 <homekhabar@gmail.com>
2026-03-08 16:43:26 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+// Load packages
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // to parse JSON requests
 
+// Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
+// Simple route to test
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
