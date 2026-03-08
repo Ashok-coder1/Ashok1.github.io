04d7635b70de6f142e0959925bde91490dae2e1b
Ashok-coder1 <homekhabar@gmail.com>
2026-03-08 15:37:24 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
