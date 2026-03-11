7a8780d69a8b7b93aebbcd1bb90365511116e306
Ashok-coder1 <homekhabar@gmail.com>
2026-03-11 20:51:11 +0545

    Update server.js

--- server.js
+++ server.js
@@ -90,20 +90,16 @@ io.on("connection", (socket) => {
     io.emit("online-users", Object.keys(onlineUsers));
   });
 
-  // --- FIXED: AGGREGATED UNREAD COUNT LOGIC ---
   socket.on("getUnreadCount", async (userId) => {
     try {
-      // This groups messages by the sender ('from') so we know who sent how many
       const unreadData = await Message.aggregate([
         { $match: { to: userId, seen: false } },
         { $group: { _id: "$from", count: { $sum: 1 } } }
       ]);
-      
-      // Sends an array: [{ _id: "senderId", count: 3 }, ...]
       socket.emit("unreadCount", unreadData);
     } catch (err) {
       console.error("Error counting unread:", err);
-      socket.emit("unreadCount", []); // Send empty array on error to prevent UI crash
+      socket.emit("unreadCount", []);
     }
   });
 
